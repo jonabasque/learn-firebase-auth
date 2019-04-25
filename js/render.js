ac7ebecde04c9936ced1a9e06accd7eab3da412f
Jon Esteban <jonmultimedia@gmail.com>
2019-04-25 05:17:40 +0200

    The supported providers will be passed in an array & created trigger elems dynamically

--- js/render.js
+++ js/render.js
@@ -4,11 +4,13 @@ function Render (elems) {
   this.elems.user.name = document.querySelector(`#${this.elems.user.name}`)
   this.elems.user.email = document.querySelector(`#${this.elems.user.email}`)
   this.elems.actions.logout = document.querySelector(`#${this.elems.actions.logout}`)
-
+  // this.elems.providers = {}
+  this._setProviders(this.elems.providers)
   return this
 }
 
 Render.prototype.user = function (user) {
+  console.log('Render.user() :', user)
   if (!user) {
     // console.log(this.elems, user)
     for (const elem of Object.values(this.elems.user)) {
@@ -19,4 +21,10 @@ Render.prototype.user = function (user) {
     this.elems.user.name.innerHTML = user.name
     this.elems.user.email.innerHTML = user.email
   }
+}
+
+Render.prototype._setProviders = function (providers) {
+  providers.forEach((provider) => {
+    this.elems[provider] = document.querySelector(`#oauth_${provider}`)
+  })
 }
